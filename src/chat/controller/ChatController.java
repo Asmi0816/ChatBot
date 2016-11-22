622347e0f72a170f35e2363a72f9a542c2150b99
Arick Smith <kidwriter2020@gmail.com>
2016-11-22 12:59:46 -0700

    Added tho file to see the html of the project.

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -38,28 +38,50 @@ public class ChatController
 	public String useChatbotCheckers(String input)
 	{
 		String answer = "";
+		if(!stupidBot.quitChecker(input))
+		{
+			if (stupidBot.contentChecker(input))
+			{
+				answer += "\nYou know my special secret\n";
+			}
+			if(stupidBot.memeChecker(input))
+			{
+				answer += "\nI can has memes?\n";
+			}
+			if(stupidBot.politicalTopicChecker(input))
+			{
+				answer += "\nAn answer?\n";
+			}
 		
-		if (stupidBot.contentChecker(input))
-		{
-			answer += "\nYou know my special secret\n";
+			if(!stupidBot.lengthChecker(answer))
+			{
+				answer += "Sorry, I don't know about " + input;
+			}
+			
+			int canBeRandom = (int)(Math.random() * 7);
+			if (canBeRandom % 7 == 0)
+			{
+				answer += randomTopicGenerator();
+			}
 		}
-		if(stupidBot.memeChecker(input))
+		else
 		{
-			answer += "\nI can has memes?\n";
-		}
-		if(stupidBot.politicalTopicChecker(input))
-		{
-			answer += "\nAn answer?\n";
-		}
-		
-		
-		if(!stupidBot.lengthChecker(answer))
-		{
-			answer += "Sorry, I don't know about " + input;
+			chatView.displayMessage("thanks for playing");
+			System.exit(0);
 		}
 		
 		
 		return answer;
+	}
+	
+	public String randomTopicGenerator()
+	{
+		String randomTopic = "";
+		
+		
+		
+		return randomTopic;
+		
 	}
 	
 	public Chatbot getChatbot()
